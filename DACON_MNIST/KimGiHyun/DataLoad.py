5b8aed8330fdea5878b8b21164ae5d492956be6c
kimh060612 <kimh060612@naver.com>
2020-08-26 12:31:58 +0900

    .

--- DACON_MNIST/KimGiHyun/DataLoad.py
+++ DACON_MNIST/KimGiHyun/DataLoad.py
@@ -7,7 +7,7 @@ test_file_path = "./test.csv"
 DigitIndex = ["0","1","2","3","4","5","6","7","8","9"]
 LetterIndex = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 
-train = False
+train = True
 
 if train :
     with open(train_file_path, "r") as File:
--- DACON_MNIST/KimGiHyun/train.py
+++ DACON_MNIST/KimGiHyun/train.py
@@ -4,13 +4,13 @@ from Model import ResidualBlock, ResNetModel
 import numpy as np
 import os
 
-EPOCHS = 1000
+EPOCHS = 400
 
 if __name__ == "__main__":
     os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
     Train_data = np.load("Image.npy")
     Train_target_digit = np.load("TargetDigit.npy")
-    Train_target_letter = np.load("TargetLetter.npy")   
+    Train_target_letter = np.load("TargetLetter.npy")
     #Input = tf.keras.Input((28, 28, 1), name="input_1")
     ResNet = ResNetModel(output_dim1=10, output_dim2=26)
     ResNet.build(input_shape=(1, 28, 28, 1))
