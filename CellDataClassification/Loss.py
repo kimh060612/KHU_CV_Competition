23647c3ac2572d7a1174081e482b4c0519b9e19e
Gihyun Kim <kimh060612@ddhinc.net>
2021-03-07 13:07:26 +0900

    .

--- CellDataClassification/Loss.py
+++ CellDataClassification/Loss.py
@@ -6,3 +6,6 @@ class AsymetricLoss(keras.layers.Layer):
     def __init__(self, trainable=True, name=None, dtype=None, dynamic=False, **kwargs):
         super().__init__(trainable=trainable, name=name, dtype=dtype, dynamic=dynamic, **kwargs)
         
+    
+    def call(self, y_true, y_pred):
+        pass
---
+++ CellDataClassification/train.py
@@ -0,0 +1,7 @@
+import numpy as np
+from tensorflow import keras
+import tensorflow as tf
+
+# Cell Data를 결국 TFRecord로 불러와야함.
+# *** 이미지의 이름 ==> output에 대한 알고리즘 필요.
+
