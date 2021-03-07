c89337b9331ea1a2848031a62ea7257438aa5a24
kimh060612 <kimh060612@naver.com>
2021-03-07 11:20:53 +0900

    .

---
+++ DACON_MNIST/KimGiHyun/test.py
@@ -0,0 +1,14 @@
+class A:
+    def __init__(self):
+        pass
+    
+    def get(self):
+        print("A")
+
+class B(A):
+    def __init__(self):
+        pass
+
+    def get(self):
+        print("B")
+
--- DACON_MNIST/KimGiHyun/train.py
+++ DACON_MNIST/KimGiHyun/train.py
@@ -5,7 +5,7 @@ import numpy as np
 import os
 from sklearn.model_selection import KFold
 
-EPOCHS = 350
+EPOCHS = 250
 
 if __name__ == "__main__":
     os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
@@ -16,11 +16,15 @@ if __name__ == "__main__":
     ResNet = ResNetModel(output_dim1=10, output_dim2=26)
     ResNet.build(input_shape=(1, 28, 28, 1))
     ResNet.summary()
-
     optimizer = tf.keras.optimizers.Adam(lr=0.001)
     CrossEntropyLoss = tf.keras.losses.CategoricalCrossentropy()
 
     ResNet.compile(optimizer, loss=[CrossEntropyLoss, CrossEntropyLoss], loss_weights=[0.7, 0.3], metrics=['accuracy', 'accuracy'])
+    optimizer = tf.keras.optimizers.Adam(lr=0.0025)
+    CrossEntropyLoss = tf.keras.losses.CategoricalCrossentropy()
+    ES = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=20)
+    #MC = tf.keras.callbacks.ModelCheckpoint('best_model.h5', monitor='val_loss')
+    ResNet.compile(optimizer, loss=[CrossEntropyLoss, CrossEntropyLoss], loss_weights=[0.5, 0.5], metrics=['accuracy', 'accuracy'])
 
     k = 8
     kfold = KFold(n_splits=k, random_state=777)
@@ -29,7 +33,7 @@ if __name__ == "__main__":
         train_letter, val_letter = Train_target_letter[train_index], Train_target_letter[validation_index]
         train_digit, val_digit = Train_target_digit[train_index], Train_target_digit[validation_index]
 
-        ResNet.fit(train_x, [train_digit, train_letter], epochs=EPOCHS, batch_size=64, validation_data=(val_x, [val_digit, val_letter]))
+        ResNet.fit(train_x, [train_digit, train_letter], epochs=EPOCHS, batch_size=64, validation_data=(val_x, [val_digit, val_letter]), callbacks=[ES])
 
 
     tf.saved_model.save(ResNet, "model")
